c2ae0748ff2b234788441a73643cdee26a11085a
HollowAOSP <ahmedshahinx0x@gmail.com>
2025-12-11 08:40:15 -0500

    fogorow: Import stock rootdir
    Change-Id: Ib0717f9aa10eec00b833f50b601f121a56c9de8b

--- device.mk
+++ device.mk
@@ -7,5 +7,9 @@
 # Inherit from mt6768-common
 $(call inherit-product, device/motorola/mt6768-common/mt6768.mk)
 
+# Rootdir
+PRODUCT_PACKAGES += \
+    init.fogorow.rc \
+
 # Inherit the proprietary files
 $(call inherit-product, vendor/motorola/fogorow/fogorow-vendor.mk)
